d6974ff8d9261619c23b286226de420c0b9128aa
Alessandro Scarozza <xan.scale@gmail.com>
2018-07-10 15:08:46 +0200

    add PATTERN_SSN_IT

--- app/src/main/java/localhost/toolkit/text/ErrorRegexListener.java
+++ app/src/main/java/localhost/toolkit/text/ErrorRegexListener.java
@@ -11,6 +11,7 @@ public class ErrorRegexListener implements OnFocusChangeListener, ErrorListenerI
 	public static final Pattern PATTERN_EMAIL = Pattern.compile("^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	public static final Pattern PATTERN_NOTEMPTY = Pattern.compile(".+");
 	public static final Pattern PATTERN_DATE_DDMMYYYY = Pattern.compile("^(0?[1-9]|[12][0-9]|3[01])[\\/\\-](0?[1-9]|1[012])[\\/\\-]\\d{4}$");
+	public static final Pattern PATTERN_SSN_IT = Pattern.compile("^(?:(?:[B-DF-HJ-NP-TV-Z]|[AEIOU])[AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}[\\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[1256LMRS][\\dLMNP-V])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM])(?:[A-MZ][1-9MNP-V][\\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$");
 	private EditText editText;
 	private Pattern pattern;
 	private String errorMsg;
