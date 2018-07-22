f6bb071091b575ba1cdc66e7c784238372cc76be
Alessandro Scarozza <xan.scale@gmail.com>
2018-07-22 17:39:15 +0200

    VisionTextDetectorView fix

--- visiontextdetectorview/src/main/java/it/localhostsoftware/visiontextdetectorview/VisionTextDetectorView.java
+++ visiontextdetectorview/src/main/java/it/localhostsoftware/visiontextdetectorview/VisionTextDetectorView.java
@@ -2,32 +2,26 @@ package it.localhostsoftware.visiontextdetectorview;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.media.ExifInterface;
 import android.util.AttributeSet;
 
-import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraUtils;
 import com.otaliastudios.cameraview.CameraView;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-public class VisionTextDetectorView extends CameraView implements Runnable {
+public class VisionTextDetectorView extends CameraView implements Runnable, CameraUtils.BitmapCallback {
 	private Handler handler;
 	private List<Pattern> patterns;
 	private Callback callback;
@@ -49,8 +43,7 @@ public class VisionTextDetectorView extends CameraView implements Runnable {
 		addCameraListener(new CameraListener() {
 			@Override
 			public void onPictureTaken(byte[] picture) {
-				new VisionTextDetector(VisionTextDetectorView.this).execute(picture);
-				//	CameraUtils.decodeBitmap(picture, );
+				CameraUtils.decodeBitmap(picture, VisionTextDetectorView.this);
 			}
 		});
 	}
@@ -89,92 +82,36 @@ public class VisionTextDetectorView extends CameraView implements Runnable {
 		super.stop();
 	}
 
-	private Bitmap decodeByteArray(byte[] data) throws IOException {
-		Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-		ByteArrayInputStream bais = new ByteArrayInputStream(data);
-		int orientation = new ExifInterface(bais).getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
-		bais.close();
-		if (orientation != ExifInterface.ORIENTATION_UNDEFINED && orientation != ExifInterface.ORIENTATION_NORMAL) {
-			Matrix matrix = new Matrix();
-			switch (orientation) {
-				case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-					matrix.setScale(-1, 1);
-					break;
-				case ExifInterface.ORIENTATION_ROTATE_180:
-					matrix.setRotate(180);
-					break;
-				case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-					matrix.setRotate(180);
-					matrix.postScale(-1, 1);
-					break;
-				case ExifInterface.ORIENTATION_TRANSPOSE:
-					matrix.setRotate(90);
-					matrix.postScale(-1, 1);
-					break;
-				case ExifInterface.ORIENTATION_ROTATE_90:
-					matrix.setRotate(90);
-					break;
-				case ExifInterface.ORIENTATION_TRANSVERSE:
-					matrix.setRotate(-90);
-					matrix.postScale(-1, 1);
-					break;
-				case ExifInterface.ORIENTATION_ROTATE_270:
-					matrix.setRotate(-90);
-					break;
-			}
-			Bitmap temp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-			bitmap.recycle();
-			bitmap = temp;
-		}
+	@Override public void onBitmapReady(Bitmap bitmap) {
 		if (r != null) {
 			Bitmap temp = Bitmap.createBitmap(bitmap, r.getX(bitmap.getWidth()), r.getY(bitmap.getHeight()), r.getWidth(bitmap.getWidth()), r.getHeight(bitmap.getHeight()));
 			bitmap.recycle();
 			bitmap = temp;
 		}
-		return bitmap;
-	}
-
-	public interface Callback {
-		void onTextDetected(VisionTextResult visionTextResult);
-	}
-
-	private static class VisionTextDetector extends AsyncTask<byte[], Void, VisionTextResult> {
-		private WeakReference<VisionTextDetectorView> ref;
-
-		VisionTextDetector(VisionTextDetectorView view) {
-			this.ref = new WeakReference<>(view);
-		}
-
-		@Override protected VisionTextResult doInBackground(byte[]... data) {
-			VisionTextDetectorView view = ref.get();
-			if (view != null)
-				try {
-					VisionTextResult visionTextResult = new VisionTextResult(view.decodeByteArray(data[0]));
-					FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(visionTextResult.bitmap);
-					FirebaseVisionText firebaseVisionText = Tasks.await(FirebaseVision.getInstance().getVisionTextDetector().detectInImage(image));
+		try {
+			final VisionTextResult visionTextResult = new VisionTextResult(bitmap);
+			FirebaseVision.getInstance().getVisionTextDetector().detectInImage(FirebaseVisionImage.fromBitmap(bitmap)).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+				@Override public void onSuccess(FirebaseVisionText firebaseVisionText) {
 					for (FirebaseVisionText.Block block : firebaseVisionText.getBlocks())
 						for (FirebaseVisionText.Line line : block.getLines())
 							for (FirebaseVisionText.Element element : line.getElements())
-								for (Pattern pattern : view.patterns)
+								for (Pattern pattern : patterns)
 									if (pattern.matcher(element.getText()).matches()) {
 										visionTextResult.matched.add(element.getText());
 										break;
 									}
-					return visionTextResult;
-				} catch (Exception e) {
-					e.printStackTrace();
+					if (!visionTextResult.matched.isEmpty())
+						callback.onTextDetected(visionTextResult);
 				}
-			return null;
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		detectInCamera();
+	}
 
-		@Override protected void onPostExecute(VisionTextResult visionTextResult) {
-			VisionTextDetectorView view = ref.get();
-			if (view != null) {
-				if (visionTextResult != null && !visionTextResult.matched.isEmpty())
-					view.callback.onTextDetected(visionTextResult);
-				view.detectInCamera();
-			}
-		}
+	public interface Callback {
+		void onTextDetected(VisionTextResult visionTextResult);
 	}
 
 	public static class Rect {
