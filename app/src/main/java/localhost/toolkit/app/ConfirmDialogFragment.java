c55baae6e4896c4608feaf2267a959f8f4339eea
Alessandro Scarozza <xan.scale@gmail.com>
2018-11-09 15:19:41 +0100

    add ConfirmDialogFragment button texts param

--- app/src/main/java/localhost/toolkit/app/ConfirmDialogFragment.java
+++ app/src/main/java/localhost/toolkit/app/ConfirmDialogFragment.java
@@ -20,28 +20,26 @@ public class ConfirmDialogFragment extends DialogFragment {
 	private static final String KEY_MSG = "message";
 	private static final String KEY_TITLE = "title";
 	private static final String KEY_EXTRA = "extra";
-	private static final String KEY_NBT = "neutralButtonText";
+	private static final String KEY_BT_POS = "positiveButtonText";
+	private static final String KEY_BT_NEG = "negativeButtonText";
+	private static final String KEY_BT_NEU = "neutralButtonText";
 
 	public static ConfirmDialogFragment newInstance(Serializable extra, String title, String msg) {
-		ConfirmDialogFragment fragment = new ConfirmDialogFragment();
-		fragment.setArguments(getBundle(extra, title, msg, null, null));
-		return fragment;
+		return newInstance(extra, title, msg, null, null, null, null);
 	}
 
-	public static ConfirmDialogFragment newInstance(Serializable extra, String title, String msg, Bitmap icon, String neutralButtonText) {
+	public static ConfirmDialogFragment newInstance(Serializable extra, String title, String msg, Bitmap icon, String positiveButtonText, String negativeButtonText, String neutralButtonText) {
 		ConfirmDialogFragment fragment = new ConfirmDialogFragment();
-		fragment.setArguments(getBundle(extra, title, msg, icon, neutralButtonText));
-		return fragment;
-	}
-
-	private static Bundle getBundle(Serializable extra, String title, String msg, Bitmap icon, String neutralButtonText) {
 		Bundle args = new Bundle();
 		args.putString(KEY_TITLE, title);
 		args.putString(KEY_MSG, msg);
-		args.putString(KEY_NBT, neutralButtonText);
+		args.putString(KEY_BT_POS, positiveButtonText);
+		args.putString(KEY_BT_NEG, negativeButtonText);
+		args.putString(KEY_BT_NEU, neutralButtonText);
 		args.putSerializable(KEY_EXTRA, extra);
 		args.putParcelable(KEY_ICON, icon);
-		return args;
+		fragment.setArguments(args);
+		return fragment;
 	}
 
 	@Override
@@ -49,19 +47,21 @@ public class ConfirmDialogFragment extends DialogFragment {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		String title = getArguments().getString(KEY_TITLE);
 		String msg = getArguments().getString(KEY_MSG);
-		String neutralButtonText = getArguments().getString(KEY_NBT);
+		String positiveButtonText = getArguments().getString(KEY_BT_POS);
+		String negativeButtonText = getArguments().getString(KEY_BT_NEG);
+		String neutralButtonText = getArguments().getString(KEY_BT_NEU);
 		Bitmap icon = getArguments().getParcelable(KEY_ICON);
 		builder.setTitle(title);
 		builder.setMessage(msg);
 		if (icon != null)
 			builder.setIcon(new BitmapDrawable(getResources(), icon));
-		builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
+		builder.setPositiveButton(positiveButtonText == null ? getString(android.R.string.ok) : positiveButtonText, new OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				getOnConfirmedListener().onConfirmation(getArguments().getSerializable(KEY_EXTRA), DialogInterface.BUTTON_POSITIVE);
 			}
 		});
-		builder.setNegativeButton(android.R.string.cancel, new OnClickListener() {
+		builder.setNegativeButton(negativeButtonText == null ? getString(android.R.string.cancel) : negativeButtonText, new OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				getOnConfirmedListener().onConfirmation(getArguments().getSerializable(KEY_EXTRA), DialogInterface.BUTTON_NEGATIVE);
