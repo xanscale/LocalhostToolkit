339251564c289516e7f0238bbfe8b17a6cdc8b57
Alessandro Scarozza <xan.scale@gmail.com>
2019-05-01 16:54:41 +0200

    refactor ItemsDialogFragment

--- app/src/main/java/localhost/toolkit/app/ItemsDialogFragment.java
+++ app/src/main/java/localhost/toolkit/app/ItemsDialogFragment.java
@@ -4,60 +4,83 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
-import java.io.Serializable;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import java.io.Serializable;
+
 public class ItemsDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
-	private static final String KEY_TITLE = "KEY_TITLE";
-	private static final String KEY_EXTRA = "KEY_EXTRA";
-	private static final String KEY_LIST_RESID = "KEY_LIST_RESID";
-	private static final String KEY_LIST_STRGS = "KEY_LIST_STRGS";
+    private static final String KEY_TITLE = "KEY_TITLE";
+    private static final String KEY_EXTRA = "KEY_EXTRA";
+    private static final String KEY_LIST_RESID = "KEY_LIST_RESID";
+    private static final String KEY_LIST_STRGS = "KEY_LIST_STRGS";
 
-	public static ItemsDialogFragment newInstance(Serializable extra, int title, int list) {
-		Bundle args = new Bundle();
-		args.putSerializable(KEY_EXTRA, extra);
-		args.putInt(KEY_TITLE, title);
-		args.putInt(KEY_LIST_RESID, list);
-		ItemsDialogFragment fragment = new ItemsDialogFragment();
-		fragment.setArguments(args);
-		return fragment;
-	}
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        assert getActivity() != null;
+        assert getArguments() != null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getArguments().getInt(KEY_TITLE));
+        if (getArguments().containsKey(KEY_LIST_STRGS))
+            builder.setItems(getArguments().getStringArray(KEY_LIST_STRGS), this);
+        else
+            builder.setItems(getArguments().getInt(KEY_LIST_RESID), this);
+        return builder.create();
+    }
 
-	public static ItemsDialogFragment newInstance(Serializable extra, int title, String[] list) {
-		Bundle args = new Bundle();
-		args.putSerializable(KEY_EXTRA, extra);
-		args.putInt(KEY_TITLE, title);
-		args.putStringArray(KEY_LIST_STRGS, list);
-		ItemsDialogFragment fragment = new ItemsDialogFragment();
-		fragment.setArguments(args);
-		return fragment;
-	}
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        assert getActivity() != null;
+        assert getArguments() != null;
+        OnListDialogClickListener l = (OnListDialogClickListener) getParentFragment();
+        if (l == null)
+            l = (OnListDialogClickListener) getActivity();
+        l.onClick(getArguments().getSerializable(KEY_EXTRA), which);
+    }
 
-	@NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-		assert getActivity() != null;
-		assert getArguments() != null;
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setTitle(getArguments().getInt(KEY_TITLE));
-		if (getArguments().getStringArray(KEY_LIST_STRGS) != null)
-			builder.setItems(getArguments().getStringArray(KEY_LIST_STRGS), this);
-		else
-			builder.setItems(getArguments().getInt(KEY_LIST_RESID), this);
-		return builder.create();
-	}
+    public interface OnListDialogClickListener {
+        void onClick(Serializable extra, int which);
+    }
 
-	@Override public void onClick(DialogInterface dialog, int which) {
-		assert getActivity() != null;
-		assert getArguments() != null;
-		OnListDialogClickListener l = (OnListDialogClickListener) getParentFragment();
-		if (l == null)
-			l = (OnListDialogClickListener) getActivity();
-		l.onClick(getArguments().getSerializable(KEY_EXTRA), which);
-	}
+    public class Builder {
+        private Serializable extra;
+        private int title;
+        private int listResId;
+        private String[] list;
 
-	public interface OnListDialogClickListener {
-		void onClick(Serializable extra, int which);
-	}
+        public ItemsDialogFragment build() {
+            ItemsDialogFragment fragment = new ItemsDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt(KEY_TITLE, title);
+            args.putInt(KEY_LIST_RESID, listResId);
+            if (list != null)
+                args.putStringArray(KEY_LIST_STRGS, list);
+            if (extra != null)
+                args.putSerializable(KEY_EXTRA, extra);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public Builder withExtra(Serializable extra) {
+            this.extra = extra;
+            return this;
+        }
+
+        public Builder withTitle(int title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder withListResId(int listResId) {
+            this.listResId = listResId;
+            return this;
+        }
+
+        public Builder withList(String[] list) {
+            this.list = list;
+            return this;
+        }
+    }
 }
