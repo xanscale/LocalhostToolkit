3933305f37ec3add68fe4c2a1c00642f5efafd4e
Alessandro Scarozza <xan.scale@gmail.com>
2019-05-01 18:06:48 +0200

    refactor TimePickerDialogFragment

--- app/src/main/java/localhost/toolkit/app/TimePickerDialogFragment.java
+++ app/src/main/java/localhost/toolkit/app/TimePickerDialogFragment.java
@@ -6,40 +6,53 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
 
-import java.util.Calendar;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class TimePickerDialogFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
-	private static final String DATE = "DATE";
+    private static final String TIME_IN_MILLIS = "TIME_IN_MILLIS";
 
-	public static TimePickerDialogFragment newInstance(long date) {
-		Bundle args = new Bundle();
-		args.putLong(DATE, date);
-		TimePickerDialogFragment fragment = new TimePickerDialogFragment();
-		fragment.setArguments(args);
-		return fragment;
-	}
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        assert getActivity() != null;
+        assert getArguments() != null;
+        Calendar c = Calendar.getInstance();
+        if (getArguments().containsKey(TIME_IN_MILLIS))
+            c.setTimeInMillis(getArguments().getLong(TIME_IN_MILLIS));
+        return new TimePickerDialog(getActivity(), this, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), DateFormat.is24HourFormat(getActivity()));
+    }
 
-	@NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-		assert getActivity() != null;
-		assert getArguments() != null;
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(getArguments().getLong(DATE));
-		return new TimePickerDialog(getActivity(), this, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), DateFormat.is24HourFormat(getActivity()));
-	}
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        assert getActivity() != null;
+        if (getParentFragment() != null)
+            ((OnTimeSetListener) getParentFragment()).onTimeSet(getTag(), hourOfDay, minute);
+        else
+            ((OnTimeSetListener) getActivity()).onTimeSet(getTag(), hourOfDay, minute);
+    }
 
-	@Override
-	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-		assert getActivity() != null;
-		if (getParentFragment() != null)
-			((OnTimeSetListener) getParentFragment()).onTimeSet(getTag(), hourOfDay, minute);
-		else
-			((OnTimeSetListener) getActivity()).onTimeSet(getTag(), hourOfDay, minute);
-	}
+    public interface OnTimeSetListener {
+        void onTimeSet(String tag, int hourOfDay, int minute);
+    }
 
-	public interface OnTimeSetListener {
-		void onTimeSet(String tag, int hourOfDay, int minute);
-	}
+    public static class Builder {
+        private Date date;
+
+        public TimePickerDialogFragment build() {
+            TimePickerDialogFragment fragment = new TimePickerDialogFragment();
+            Bundle args = new Bundle();
+            if (date != null) args.putLong(TIME_IN_MILLIS, date.getTime());
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public Builder withDate(Date date) {
+            this.date = date;
+            return this;
+        }
+    }
 }
