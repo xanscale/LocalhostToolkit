1d05e21e58058b126caa7f451c9033ced23892ee
Alessandro Scarozza <xan.scale@gmail.com>
2019-05-01 16:13:08 +0200

    refactor DatePickerDialogFragment

--- app/src/main/java/localhost/toolkit/app/DatePickerDialogFragment.java
+++ app/src/main/java/localhost/toolkit/app/DatePickerDialogFragment.java
@@ -5,39 +5,52 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.DatePicker;
 
-import java.util.Calendar;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DatePickerDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
-	private static final String DATE = "DATE";
+    private static final String DATE = "DATE";
 
-	public static DatePickerDialogFragment newInstance(long date) {
-		Bundle args = new Bundle();
-		args.putLong(DATE, date);
-		DatePickerDialogFragment fragment = new DatePickerDialogFragment();
-		fragment.setArguments(args);
-		return fragment;
-	}
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        assert getActivity() != null;
+        assert getArguments() != null;
+        c.setTimeInMillis(getArguments().getLong(DATE));
+        return new DatePickerDialog(getActivity(), this, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+    }
 
-	@NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-		Calendar c = Calendar.getInstance();
-		assert getActivity() != null;
-		assert getArguments() != null;
-		c.setTimeInMillis(getArguments().getLong(DATE));
-		return new DatePickerDialog(getActivity(), this, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
-	}
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        assert getActivity() != null;
+        if (getParentFragment() != null)
+            ((OnDateSetListener) getParentFragment()).onDateSet(getTag(), year, monthOfYear, dayOfMonth);
+        else
+            ((OnDateSetListener) getActivity()).onDateSet(getTag(), year, monthOfYear, dayOfMonth);
+    }
 
-	@Override public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-		assert getActivity() != null;
-		if (getParentFragment() != null)
-			((OnDateSetListener) getParentFragment()).onDateSet(getTag(), year, monthOfYear, dayOfMonth);
-		else
-			((OnDateSetListener) getActivity()).onDateSet(getTag(), year, monthOfYear, dayOfMonth);
-	}
+    public interface OnDateSetListener {
+        void onDateSet(String tag, int year, int monthOfYear, int dayOfMonth);
+    }
 
-	public interface OnDateSetListener {
-		void onDateSet(String tag, int year, int monthOfYear, int dayOfMonth);
-	}
+    public class Builder {
+        private Date date;
+
+        public DatePickerDialogFragment build() {
+            DatePickerDialogFragment fragment = new DatePickerDialogFragment();
+            Bundle args = new Bundle();
+            args.putLong(DATE, date.getTime());
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public Builder withDate(Date date) {
+            this.date = date;
+            return this;
+        }
+    }
 }
