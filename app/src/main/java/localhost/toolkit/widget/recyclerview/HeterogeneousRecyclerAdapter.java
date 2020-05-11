7f4df05e2d56a01c35d388dca9c4ae0568b73786
Alessandro Scarozza <xan.scale@gmail.com>
2020-05-11 17:08:01 +0200

    add loopScroll to HeterogeneousRecyclerAdapter

--- app/src/main/java/localhost/toolkit/widget/recyclerview/HeterogeneousRecyclerAdapter.java
+++ app/src/main/java/localhost/toolkit/widget/recyclerview/HeterogeneousRecyclerAdapter.java
@@ -7,102 +7,119 @@ import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
 public class HeterogeneousRecyclerAdapter<I extends HeterogeneousRecyclerItem> extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
-	private HeterogeneousFilter heterogeneousFilter;
-	private HashMap<Class, Integer> classToType;
-	private SparseIntArray typeToPos;
-	private LayoutInflater inflater;
-	private List<I> items;
-	private List<I> originalItems;
+    private HeterogeneousFilter heterogeneousFilter;
+    private HashMap<Class, Integer> classToType;
+    private SparseIntArray typeToPos;
+    private LayoutInflater inflater;
+    private List<I> items;
+    private List<I> originalItems;
+    private boolean loopScroll;
 
-	public HeterogeneousRecyclerAdapter(Context context, List<I> items) {
-		setHasStableIds(true);
-		this.items = items;
-		inflater = LayoutInflater.from(context);
-		classToType = new HashMap<>();
-		typeToPos = new SparseIntArray();
-		notifyTypesChanged();
-	}
+    public HeterogeneousRecyclerAdapter(Context context, List<I> items) {
+        setHasStableIds(true);
+        this.items = items;
+        inflater = LayoutInflater.from(context);
+        classToType = new HashMap<>();
+        typeToPos = new SparseIntArray();
+        notifyTypesChanged();
+    }
 
-	@NonNull @Override public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-		return items.get(typeToPos.get(viewType)).onCreateViewHolder(inflater, parent);
-	}
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return getItem(typeToPos.get(viewType)).onCreateViewHolder(inflater, parent);
+    }
 
-	@Override public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
-		items.get(position).onBindViewHolder(viewHolder);
-	}
+    @Override
+    @SuppressWarnings("unchecked")
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        getItem(position).onBindViewHolder(viewHolder);
+    }
 
-	public I getItem(int position) {
-		return items.get(position);
-	}
+    public I getItem(int position) {
+        return items.get(loopScroll ? position % items.size() : position);
+    }
 
-	@Override public int getItemCount() {
-		return items.size();
-	}
+    @Override
+    public int getItemCount() {
+        return loopScroll ? Integer.MAX_VALUE : items.size();
+    }
 
-	@Override public int getItemViewType(int position) {
-		Integer type = classToType.get(items.get(position).getClass());
-		return type == null ? super.getItemViewType(position) : type;
-	}
+    @Override
+    public int getItemViewType(int position) {
+        Integer type = classToType.get(getItem(position).getClass());
+        return type == null ? super.getItemViewType(position) : type;
+    }
 
-	@Override public long getItemId(int position) {
-		return getItem(position).hashCode();
-	}
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).hashCode();
+    }
 
-	public void notifyTypesChanged() {
-		typeToPos.clear();
-		for (int i = 0; i < items.size(); i++) {
-			if (!classToType.containsKey(items.get(i).getClass()))
-				classToType.put(items.get(i).getClass(), classToType.size());
-			Integer type = classToType.get(items.get(i).getClass());
-			if (type != null && typeToPos.indexOfKey(type) < 0)
-				typeToPos.put(type, i);
-		}
-		notifyDataSetChanged();
-	}
+    public void notifyTypesChanged() {
+        typeToPos.clear();
+        for (int i = 0; i < items.size(); i++) {
+            if (!classToType.containsKey(items.get(i).getClass()))
+                classToType.put(items.get(i).getClass(), classToType.size());
+            Integer type = classToType.get(items.get(i).getClass());
+            if (type != null && typeToPos.indexOfKey(type) < 0)
+                typeToPos.put(type, i);
+        }
+        notifyDataSetChanged();
+    }
 
-	@Override public @NonNull Filter getFilter() {
-		if (heterogeneousFilter == null)
-			heterogeneousFilter = new HeterogeneousFilter();
-		return heterogeneousFilter;
-	}
+    @Override
+    public @NonNull
+    Filter getFilter() {
+        if (heterogeneousFilter == null)
+            heterogeneousFilter = new HeterogeneousFilter();
+        return heterogeneousFilter;
+    }
 
-	private class HeterogeneousFilter extends Filter {
-		@Override protected FilterResults performFiltering(CharSequence constraint) {
-			FilterResults results = new FilterResults();
-			if (originalItems == null)
-				originalItems = new ArrayList<>(items);
-			if (constraint == null || constraint.length() == 0) {
-				results.values = new ArrayList<>(originalItems);
-				results.count = originalItems.size();
-			} else {
-				StringBuilder sb = new StringBuilder();
-				sb.append("^\\X*");
-				for (String word : constraint.toString().toLowerCase().split("\\W"))
-					sb.append("(?=.*").append(word).append(")");
-				sb.append("\\X*$");
-				ArrayList<I> newValues = new ArrayList<>();
-				Pattern pattern = Pattern.compile(sb.toString());
-				for (I value : originalItems)
-					if (pattern.matcher(value.toString().toLowerCase()).matches())
-						newValues.add(value);
-				results.values = newValues;
-				results.count = newValues.size();
-			}
-			return results;
-		}
+    public void setLoopScroll(boolean loopScroll) {
+        this.loopScroll = loopScroll;
+    }
 
-		@Override protected void publishResults(CharSequence constraint, FilterResults results) {
-			items = (List<I>) results.values;
-			notifyDataSetChanged();
-		}
-	}
+    private class HeterogeneousFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if (originalItems == null)
+                originalItems = new ArrayList<>(items);
+            if (constraint == null || constraint.length() == 0) {
+                results.values = new ArrayList<>(originalItems);
+                results.count = originalItems.size();
+            } else {
+                StringBuilder sb = new StringBuilder();
+                sb.append("^\\X*");
+                for (String word : constraint.toString().toLowerCase().split("\\W"))
+                    sb.append("(?=.*").append(word).append(")");
+                sb.append("\\X*$");
+                ArrayList<I> newValues = new ArrayList<>();
+                Pattern pattern = Pattern.compile(sb.toString());
+                for (I value : originalItems)
+                    if (pattern.matcher(value.toString().toLowerCase()).matches())
+                        newValues.add(value);
+                results.values = newValues;
+                results.count = newValues.size();
+            }
+            return results;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            items = (List<I>) results.values;
+            notifyDataSetChanged();
+        }
+    }
 }
--- app/src/main/java/localhost/toolkit/widget/recyclerview/HeterogeneousRecyclerItem.java
+++ app/src/main/java/localhost/toolkit/widget/recyclerview/HeterogeneousRecyclerItem.java
@@ -3,42 +3,44 @@ package localhost.toolkit.widget.recyclerview;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import java.io.Serializable;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class HeterogeneousRecyclerItem<E, H extends RecyclerView.ViewHolder> {
-	protected E extra;
+    protected E extra;
 
-	public HeterogeneousRecyclerItem(E extra) {
-		this.extra = extra;
-	}
+    public HeterogeneousRecyclerItem(E extra) {
+        this.extra = extra;
+    }
 
-	public abstract H onCreateViewHolder(LayoutInflater inflater, ViewGroup parent);
+    public abstract H onCreateViewHolder(LayoutInflater inflater, ViewGroup parent);
 
-	public abstract void onBindViewHolder(H holder);
+    public abstract void onBindViewHolder(H holder);
 
-	public E getExtra() {
-		return extra;
-	}
+    public E getExtra() {
+        return extra;
+    }
 
-	public void setExtra(E extra) {
-		this.extra = extra;
-	}
+    public void setExtra(E extra) {
+        this.extra = extra;
+    }
 
-	@NonNull @Override public String toString() {
-		return extra.toString();
-	}
+    @NonNull
+    @Override
+    public String toString() {
+        return extra.toString();
+    }
 
-	@Override public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof HeterogeneousRecyclerItem)) return false;
-		HeterogeneousRecyclerItem<?, ?> that = (HeterogeneousRecyclerItem<?, ?>) o;
-		return extra.equals(that.extra);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HeterogeneousRecyclerItem)) return false;
+        HeterogeneousRecyclerItem<?, ?> that = (HeterogeneousRecyclerItem<?, ?>) o;
+        return extra.equals(that.extra);
+    }
 
-	@Override public int hashCode() {
-		return extra.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return extra.hashCode();
+    }
 }
