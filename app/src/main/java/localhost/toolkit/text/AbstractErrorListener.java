62c14c2bfcc317bd7dc4f64994dc8eb02d608fee
Alessandro Scarozza <xan.scale@gmail.com>
2020-05-20 17:54:58 +0200

    fix

--- app/src/main/java/localhost/toolkit/text/AbstractErrorListener.java
+++ app/src/main/java/localhost/toolkit/text/AbstractErrorListener.java
@@ -2,9 +2,16 @@ package localhost.toolkit.text;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 
-public abstract class AbstractErrorListener implements TextWatcher {
+public abstract class AbstractErrorListener implements TextWatcher, View.OnFocusChangeListener {
     abstract boolean matches(boolean hidden);
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus)
+            matches(false);
+    }
 
     @Override
     public void afterTextChanged(Editable s) {
