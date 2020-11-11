bd7b661aff397beab281f6f417bb0b6b4cc7530e
Alessandro Scarozza <xan.scale@gmail.com>
2020-11-11 15:10:26 +0100

    update ProgressDialogFragment

--- app/src/main/java/localhost/toolkit/app/fragment/ProgressDialogFragment.java
+++ app/src/main/java/localhost/toolkit/app/fragment/ProgressDialogFragment.java
@@ -1,27 +1,27 @@
 package localhost.toolkit.app.fragment;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import localhost.toolkit.R;
+
 public class ProgressDialogFragment extends DialogFragment {
-    private static final String MESSAGE = "MESSAGE";
-    private static final String TITLE = "TITLE";
     private static final String CANCELABLE = "CANCELABLE";
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        ProgressDialog pd = new ProgressDialog(requireContext());
-        pd.setTitle(requireArguments().getString(TITLE));
-        pd.setMessage(requireArguments().getString(MESSAGE));
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext(), R.style.ThemeOverlay_MaterialComponents_Dialog_Alert_App);
+        builder.setView(new ProgressBar(requireContext()));
         setCancelable((requireArguments().getBoolean(CANCELABLE, true)));
         requireActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        return pd;
+        return builder.create();
     }
 
     @Override
@@ -31,31 +31,15 @@ public class ProgressDialogFragment extends DialogFragment {
     }
 
     public static class Builder {
-        private String title;
-        private String message;
         private Boolean cancelable;
 
         public ProgressDialogFragment build() {
             ProgressDialogFragment fragment = new ProgressDialogFragment();
             Bundle args = new Bundle();
-            if (title != null)
-                args.putString(TITLE, title);
-            if (message != null)
-                args.putString(MESSAGE, message);
             if (cancelable != null)
                 args.putBoolean(CANCELABLE, cancelable);
             fragment.setArguments(args);
             return fragment;
-        }
-
-        public Builder withTitle(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public Builder withMessage(String message) {
-            this.message = message;
-            return this;
         }
 
         public Builder withCancelable(Boolean cancelable) {
