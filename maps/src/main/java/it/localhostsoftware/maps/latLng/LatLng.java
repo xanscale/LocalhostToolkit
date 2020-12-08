6ae254a5f357e09fe78b77a2e843dd48f731f0de
Alessandro Scarozza <xan.scale@gmail.com>
2020-12-08 10:15:11 +0100

    clean

--- maps/src/main/java/it/localhostsoftware/maps/latLng/LatLng.java
+++ maps/src/main/java/it/localhostsoftware/maps/latLng/LatLng.java
@@ -1,6 +1,17 @@
 package it.localhostsoftware.maps.latLng;
 
+import android.content.Context;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
 public interface LatLng {
+    static LatLng getInstance(Context context, double var1, double var3) {
+        if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS)
+            return new GoogleLatLng(var1, var3);
+        else return null;
+    }
+
     double getLatitude();
 
     double getLongitude();
