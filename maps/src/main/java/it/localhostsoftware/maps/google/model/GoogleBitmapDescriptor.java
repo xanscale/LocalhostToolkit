db9cd8f1ade20f17f057642ecc2e2b9a27a40fa9
Alessandro Scarozza <xan.scale@gmail.com>
2020-12-08 19:13:05 +0100

    clean

--- maps/src/main/java/it/localhostsoftware/maps/google/model/GoogleBitmapDescriptor.java
+++
@@ -1,9 +0,0 @@
-package it.localhostsoftware.maps.google.model;
-
-import it.localhostsoftware.maps.model.BitmapDescriptor;
-
-public class GoogleBitmapDescriptor extends BitmapDescriptor<com.google.android.gms.maps.model.BitmapDescriptor> {
-    public GoogleBitmapDescriptor(com.google.android.gms.maps.model.BitmapDescriptor bitmapDescriptor) {
-        super(bitmapDescriptor);
-    }
-}
