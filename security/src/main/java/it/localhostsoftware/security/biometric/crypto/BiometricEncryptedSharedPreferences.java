1e0d0f7c626c46bc6d679e68d94ea94a52c9d21c
Alessandro Scarozza <xan.scale@gmail.com>
2021-01-22 10:52:38 +0100

    bump libs

--- security/src/main/java/it/localhostsoftware/security/biometric/crypto/BiometricEncryptedSharedPreferences.java
+++ security/src/main/java/it/localhostsoftware/security/biometric/crypto/BiometricEncryptedSharedPreferences.java
@@ -25,14 +25,14 @@ public class BiometricEncryptedSharedPreferences {
     /**
      * @param fragment                                  A reference to the client's fragment
      * @param fileName                                  The name of the file to open; can not contain path separators
-     * @param userAuthenticationValidityDurationSeconds duration in seconds, must be greater than 0
+     * @param timeout duration in seconds, must be greater than 0
      * @param promptInfo                                The information that will be displayed on the prompt. Create this object using {@link BiometricPrompt.PromptInfo.Builder}
      * @return LiveData of EncryptedSharedPreferences that requires user biometric authentication
      */
-    public static LiveData<SharedPreferences> create(final Fragment fragment, final String fileName, final int userAuthenticationValidityDurationSeconds, BiometricPrompt.PromptInfo promptInfo) {
+    public static LiveData<SharedPreferences> create(final Fragment fragment, final String fileName, final int timeout, BiometricPrompt.PromptInfo promptInfo) {
         final MutableLiveData<SharedPreferences> out = new MutableLiveData<>();
         new BiometricPrompt(fragment.requireActivity(), ContextCompat.getMainExecutor(fragment.requireContext()),
-                new AuthenticationCallback(fragment.requireContext(), fileName, userAuthenticationValidityDurationSeconds, out)
+                new AuthenticationCallback(fragment.requireContext(), fileName, timeout, out)
         ).authenticate(promptInfo);
         return out;
     }
@@ -40,19 +40,19 @@ public class BiometricEncryptedSharedPreferences {
     /**
      * @param activity                                  A reference to the client's activity
      * @param fileName                                  The name of the file to open; can not contain path separators
-     * @param userAuthenticationValidityDurationSeconds duration in seconds, must be greater than 0
+     * @param timeout duration in seconds, must be greater than 0
      * @param promptInfo                                The information that will be displayed on the prompt. Create this object using {@link BiometricPrompt.PromptInfo.Builder}
      * @return LiveData of EncryptedSharedPreferences that requires user biometric authentication
      */
-    public static LiveData<SharedPreferences> create(final FragmentActivity activity, final String fileName, final int userAuthenticationValidityDurationSeconds, BiometricPrompt.PromptInfo promptInfo) {
+    public static LiveData<SharedPreferences> create(final FragmentActivity activity, final String fileName, final int timeout, BiometricPrompt.PromptInfo promptInfo) {
         final MutableLiveData<SharedPreferences> out = new MutableLiveData<>();
         new BiometricPrompt(activity, ContextCompat.getMainExecutor(activity),
-                new AuthenticationCallback(activity, fileName, userAuthenticationValidityDurationSeconds, out)
+                new AuthenticationCallback(activity, fileName, timeout, out)
         ).authenticate(promptInfo);
         return out;
     }
 
-    private static SharedPreferences create(Context c, String fileName, int userAuthenticationValidityDurationSeconds) {
+    private static SharedPreferences create(Context c, String fileName, int timeout) {
         try {
             return EncryptedSharedPreferences.create(
                     fileName,
@@ -63,7 +63,8 @@ public class BiometricEncryptedSharedPreferences {
                             .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
                             .setKeySize(KEY_SIZE)
                             .setUserAuthenticationRequired(true)
-                            .setUserAuthenticationValidityDurationSeconds(userAuthenticationValidityDurationSeconds).build()),
+                            .setUserAuthenticationParameters(timeout, KeyProperties.AUTH_DEVICE_CREDENTIAL | KeyProperties.AUTH_BIOMETRIC_STRONG)
+                            .build()),
                     c,
                     EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                     EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
@@ -77,20 +78,20 @@ public class BiometricEncryptedSharedPreferences {
     private static class AuthenticationCallback extends BiometricPrompt.AuthenticationCallback {
         private final Context context;
         private final String fileName;
-        private final int userAuthenticationValidityDurationSeconds;
+        private final int timeout;
         private final MutableLiveData<SharedPreferences> out;
 
-        AuthenticationCallback(Context context, String fileName, int userAuthenticationValidityDurationSeconds, MutableLiveData<SharedPreferences> out) {
+        AuthenticationCallback(Context context, String fileName, int timeout, MutableLiveData<SharedPreferences> out) {
             this.context = context;
             this.fileName = fileName;
-            this.userAuthenticationValidityDurationSeconds = userAuthenticationValidityDurationSeconds;
+            this.timeout = timeout;
             this.out = out;
         }
 
         @Override
         public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
             super.onAuthenticationSucceeded(result);
-            out.postValue(create(context, fileName, userAuthenticationValidityDurationSeconds));
+            out.postValue(create(context, fileName, timeout));
         }
 
         @Override
