2d06b39f3b66f622299f70395819f1b2e4ea0c16
Alessandro Scarozza <xan.scale@gmail.com>
2022-01-27 17:39:33 +0100

    add ImageLabeler options

--- mlkit/src/main/java/it/localhostsoftware/mlkit/vision/label/ImageLabeler.java
+++ mlkit/src/main/java/it/localhostsoftware/mlkit/vision/label/ImageLabeler.java
@@ -8,6 +8,7 @@ import androidx.camera.core.ImageProxy;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabelerOptionsBase;
 import com.google.mlkit.vision.label.ImageLabeling;
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
@@ -16,11 +17,17 @@ import java.util.List;
 import it.localhostsoftware.mlkit.vision.AbstractImageAnalyzer;
 
 public class ImageLabeler extends AbstractImageAnalyzer<List<ImageLabel>> {
+    private ImageLabelerOptionsBase options = ImageLabelerOptions.DEFAULT_OPTIONS;
+
+    public void setOptions(ImageLabelerOptionsBase options) {
+        this.options = options;
+    }
+
     @Override
     protected void process(ImageProxy imageProxy, OnSuccessListener<List<ImageLabel>> onSuccessListener) {
         @SuppressLint("UnsafeOptInUsageError") Image image = imageProxy.getImage();
         if (onSuccessListener != null && image != null)
-            ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS).process(InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees()))
+            ImageLabeling.getClient(options).process(InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees()))
                     .addOnSuccessListener(onSuccessListener)
                     .addOnFailureListener(Throwable::printStackTrace)
                     .addOnCompleteListener(task -> imageProxy.close());
