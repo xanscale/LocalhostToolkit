880a4f22a68aea4e8a08b2e4c71b77a90138a31f
Alessandro Scarozza <xan.scale@gmail.com>
2022-02-15 22:07:44 +0100

    add setScaleGestureDetector on AbstractImageAnalyzer

--- mlkit/src/main/java/it/localhostsoftware/mlkit/vision/AbstractImageAnalyzer.java
+++ mlkit/src/main/java/it/localhostsoftware/mlkit/vision/AbstractImageAnalyzer.java
@@ -4,13 +4,16 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.media.Image;
+import android.view.ScaleGestureDetector;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
+import androidx.camera.core.ZoomState;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
@@ -38,7 +41,8 @@ public abstract class AbstractImageAnalyzer<TResult> {
             ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().build();
             imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), imageProxy -> analyze(imageProxy, onSuccessListener));
             try {
-                future.get().bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis);
+                Camera camera = future.get().bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageAnalysis);
+                setScaleGestureDetector(context, camera, previewView);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -52,6 +56,21 @@ public abstract class AbstractImageAnalyzer<TResult> {
                     .addOnSuccessListener(onSuccessListener)
                     .addOnFailureListener(Throwable::printStackTrace)
                     .addOnCompleteListener(task -> imageProxy.close());
+    }
+
+    private void setScaleGestureDetector(@NonNull Context context, @NonNull Camera camera, @NonNull PreviewView previewView) {
+        ScaleGestureDetector scaleGestureDetector = new ScaleGestureDetector(context, new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                ZoomState zoomState = camera.getCameraInfo().getZoomState().getValue();
+                camera.getCameraControl().setZoomRatio(detector.getScaleFactor() * (zoomState == null ? 1.0f : zoomState.getZoomRatio()));
+                return true;
+            }
+        });
+        previewView.setOnTouchListener((v, event) -> {
+            v.performClick();
+            return scaleGestureDetector.onTouchEvent(event);
+        });
     }
 
     protected abstract Task<TResult> process(@NonNull Image image, int rotationDegrees);
