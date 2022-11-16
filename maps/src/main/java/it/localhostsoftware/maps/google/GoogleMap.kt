aa5aeed696cc1f8581189eaea6c7abab13d4b4bd
Alessandro Scarozza <xan.scale@gmail.com>
2022-11-16 20:34:29 +0100

    clean maps

--- maps/src/main/java/it/localhostsoftware/maps/google/GoogleMap.kt
+++ maps/src/main/java/it/localhostsoftware/maps/google/GoogleMap.kt
@@ -11,9 +11,8 @@ import it.localhostsoftware.maps.Projection
 import it.localhostsoftware.maps.UiSettings
 import it.localhostsoftware.maps.google.model.*
 import it.localhostsoftware.maps.model.MarkerOptions
-import it.localhostsoftware.maps.model.PolylineOptions
 
-class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, GoogleCameraPosition>(googleMap) {
+class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, GoogleCameraPosition, GooglePolylineOptions>(googleMap) {
     override val cameraPosition: GoogleCameraPosition
         get() = GoogleCameraPosition(map.cameraPosition)
     override val maxZoomLevel: Float
@@ -21,67 +20,56 @@ class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, Go
     override val minZoomLevel: Float
         get() = map.minZoomLevel
 
-    override fun moveCamera(var1: GoogleCameraUpdate) {
+    override fun moveCamera(var1: GoogleCameraUpdate) =
         map.moveCamera(var1.cu)
-    }
 
-    override fun animateCamera(var1: GoogleCameraUpdate) {
+    override fun animateCamera(var1: GoogleCameraUpdate) =
         map.animateCamera(var1.cu)
-    }
 
-    override fun animateCamera(var1: GoogleCameraUpdate, var2: CancelableCallback?) {
+    override fun animateCamera(var1: GoogleCameraUpdate, var2: CancelableCallback?) =
         map.animateCamera(var1.cu, var2?.let {
             object : GoogleMap.CancelableCallback {
                 override fun onFinish() = it.onFinish()
                 override fun onCancel() = it.onCancel()
             }
         })
-    }
 
-    override fun animateCamera(var1: GoogleCameraUpdate, var2: Int, var3: CancelableCallback?) {
+    override fun animateCamera(var1: GoogleCameraUpdate, var2: Int, var3: CancelableCallback?) =
         map.animateCamera(var1.cu, var2, var3?.let {
             object : GoogleMap.CancelableCallback {
                 override fun onFinish() = it.onFinish()
                 override fun onCancel() = it.onCancel()
             }
         })
-    }
 
-    override fun stopAnimation() {
+    override fun stopAnimation() =
         map.stopAnimation()
-    }
 
-    override fun addPolyline(var1: PolylineOptions<*>): it.localhostsoftware.maps.model.Polyline<*> {
-        return GooglePolyline(map.addPolyline(var1.po as com.google.android.gms.maps.model.PolylineOptions))
-    }
+    override fun addPolyline(var1: GooglePolylineOptions) =
+        GooglePolyline(map.addPolyline(var1.po))
 
-    override fun addPolygon(var1: it.localhostsoftware.maps.model.PolygonOptions<*>): it.localhostsoftware.maps.model.Polygon<*> {
-        return GooglePolygon(map.addPolygon(var1.po as PolygonOptions))
-    }
+    override fun addPolygon(var1: it.localhostsoftware.maps.model.PolygonOptions<*>) =
+        GooglePolygon(map.addPolygon(var1.po as PolygonOptions))
 
-    override fun addCircle(var1: it.localhostsoftware.maps.model.CircleOptions<*>): it.localhostsoftware.maps.model.Circle<*> {
-        return GoogleCircle(map.addCircle(var1.co as CircleOptions))
-    }
+    override fun addCircle(var1: it.localhostsoftware.maps.model.CircleOptions<*>) =
+        GoogleCircle(map.addCircle(var1.co as CircleOptions))
 
-    override fun addMarker(var1: MarkerOptions<*>): it.localhostsoftware.maps.model.Marker<*>? {
-        return map.addMarker((var1 as GoogleMarkerOptions).mo)?.let { GoogleMarker(it) }
-    }
+    override fun addMarker(var1: MarkerOptions<*>) =
+        map.addMarker((var1 as GoogleMarkerOptions).mo)?.let { GoogleMarker(it) }
 
-    override fun clear() {
+    override fun clear() =
         map.clear()
-    }
 
     override val focusedBuilding: it.localhostsoftware.maps.model.IndoorBuilding<*>?
         get() = map.focusedBuilding?.let { GoogleIndoorBuilding(it) }
 
-    override fun setOnIndoorStateChangeListener(var1: OnIndoorStateChangeListener?) {
+    override fun setOnIndoorStateChangeListener(var1: OnIndoorStateChangeListener?) =
         map.setOnIndoorStateChangeListener(var1?.let {
             object : GoogleMap.OnIndoorStateChangeListener {
                 override fun onIndoorBuildingFocused() = it.onIndoorBuildingFocused()
                 override fun onIndoorLevelActivated(indoorBuilding: IndoorBuilding) = it.onIndoorLevelActivated(GoogleIndoorBuilding(indoorBuilding))
             }
         })
-    }
 
     override var mapType: Int
         get() = map.mapType
@@ -96,9 +84,8 @@ class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, Go
     override val isIndoorEnabled: Boolean
         get() = map.isIndoorEnabled
 
-    override fun setIndoorEnabled(var1: Boolean): Boolean {
-        return map.setIndoorEnabled(var1)
-    }
+    override fun setIndoorEnabled(var1: Boolean) =
+        map.setIndoorEnabled(var1)
 
     override var isBuildingsEnabled: Boolean
         get() = map.isBuildingsEnabled
@@ -113,7 +100,7 @@ class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, Go
             map.isMyLocationEnabled = b
         }
 
-    override fun setLocationSource(var1: it.localhostsoftware.maps.LocationSource?) {
+    override fun setLocationSource(var1: it.localhostsoftware.maps.LocationSource?) =
         map.setLocationSource(var1?.let { ls ->
             object : LocationSource {
                 override fun deactivate() = ls.deactivate()
@@ -121,42 +108,34 @@ class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, Go
                     var1.activate(it.localhostsoftware.maps.LocationSource.OnLocationChangedListener { onLocationChangedListener.onLocationChanged(it) })
             }
         })
-    }
 
     override val uiSettings: UiSettings<*>
         get() = GoogleUiSettings(map.uiSettings)
     override val projection: Projection<*>
         get() = GoogleProjection(map.projection)
 
-    override fun setOnCameraMoveStartedListener(var1: OnCameraMoveStartedListener?) {
+    override fun setOnCameraMoveStartedListener(var1: OnCameraMoveStartedListener?) =
         map.setOnCameraMoveStartedListener(var1?.let { GoogleMap.OnCameraMoveStartedListener { var1.onCameraMoveStarted(it) } })
-    }
 
-    override fun setOnCameraMoveListener(var1: OnCameraMoveListener?) {
+    override fun setOnCameraMoveListener(var1: OnCameraMoveListener?) =
         map.setOnCameraMoveListener(var1?.let { GoogleMap.OnCameraMoveListener { var1.onCameraMove() } })
-    }
 
-    override fun setOnCameraMoveCanceledListener(var1: OnCameraMoveCanceledListener?) {
+    override fun setOnCameraMoveCanceledListener(var1: OnCameraMoveCanceledListener?) =
         map.setOnCameraMoveCanceledListener(var1?.let { GoogleMap.OnCameraMoveCanceledListener { var1.onCameraMoveCanceled() } })
-    }
 
-    override fun setOnCameraIdleListener(var1: OnCameraIdleListener?) {
+    override fun setOnCameraIdleListener(var1: OnCameraIdleListener?) =
         map.setOnCameraIdleListener(var1?.let { GoogleMap.OnCameraIdleListener { var1.onCameraIdle() } })
-    }
 
-    override fun setOnMapClickListener(var1: OnMapClickListener?) {
+    override fun setOnMapClickListener(var1: OnMapClickListener?) =
         map.setOnMapClickListener(var1?.let { GoogleMap.OnMapClickListener { var1.onMapClick(GoogleLatLng(it)) } })
-    }
 
-    override fun setOnMapLongClickListener(var1: OnMapLongClickListener?) {
+    override fun setOnMapLongClickListener(var1: OnMapLongClickListener?) =
         map.setOnMapLongClickListener(var1?.let { GoogleMap.OnMapLongClickListener { var1.onMapLongClick(GoogleLatLng(it)) } })
-    }
 
-    override fun setOnMarkerClickListener(var1: OnMarkerClickListener?) {
+    override fun setOnMarkerClickListener(var1: OnMarkerClickListener?) =
         map.setOnMarkerClickListener(var1?.let { GoogleMap.OnMarkerClickListener { var1.onMarkerClick(GoogleMarker(it)) } })
-    }
 
-    override fun setOnMarkerDragListener(var1: OnMarkerDragListener?) {
+    override fun setOnMarkerDragListener(var1: OnMarkerDragListener?) =
         map.setOnMarkerDragListener(var1?.let {
             object : GoogleMap.OnMarkerDragListener {
                 override fun onMarkerDragStart(marker: Marker) = var1.onMarkerDragStart(GoogleMarker(marker))
@@ -164,90 +143,69 @@ class GoogleMap(googleMap: GoogleMap) : GeoMap<GoogleMap, GoogleCameraUpdate, Go
                 override fun onMarkerDragEnd(marker: Marker) = var1.onMarkerDragEnd(GoogleMarker(marker))
             }
         })
-    }
 
-    override fun setOnInfoWindowClickListener(var1: OnInfoWindowClickListener?) {
+    override fun setOnInfoWindowClickListener(var1: OnInfoWindowClickListener?) =
         map.setOnInfoWindowClickListener(var1?.let { GoogleMap.OnInfoWindowClickListener { var1.onInfoWindowClick(GoogleMarker(it)) } })
-    }
 
-    override fun setOnInfoWindowLongClickListener(var1: OnInfoWindowLongClickListener?) {
+    override fun setOnInfoWindowLongClickListener(var1: OnInfoWindowLongClickListener?) =
         map.setOnInfoWindowLongClickListener(var1?.let { GoogleMap.OnInfoWindowLongClickListener { var1.onInfoWindowLongClick(GoogleMarker(it)) } })
-    }
 
-    override fun setOnInfoWindowCloseListener(var1: OnInfoWindowCloseListener?) {
+    override fun setOnInfoWindowCloseListener(var1: OnInfoWindowCloseListener?) =
         map.setOnInfoWindowCloseListener(var1?.let { GoogleMap.OnInfoWindowCloseListener { var1.onInfoWindowClose(GoogleMarker(it)) } })
-    }
 
-    override fun setInfoWindowAdapter(var1: InfoWindowAdapter?) {
+    override fun setInfoWindowAdapter(var1: InfoWindowAdapter?) =
         map.setInfoWindowAdapter(var1?.let {
             object : GoogleMap.InfoWindowAdapter {
                 override fun getInfoWindow(marker: Marker): View = var1.getInfoWindow(GoogleMarker(marker))
                 override fun getInfoContents(marker: Marker): View = var1.getInfoContents(GoogleMarker(marker))
             }
         })
-    }
 
-    override fun setOnMyLocationButtonClickListener(var1: OnMyLocationButtonClickListener?) {
+    override fun setOnMyLocationButtonClickListener(var1: OnMyLocationButtonClickListener?) =
         map.setOnMyLocationButtonClickListener(var1?.let { GoogleMap.OnMyLocationButtonClickListener { var1.onMyLocationButtonClick() } })
-    }
 
-    override fun setOnMyLocationClickListener(var1: OnMyLocationClickListener?) {
+    override fun setOnMyLocationClickListener(var1: OnMyLocationClickListener?) =
         map.setOnMyLocationClickListener(var1?.let { GoogleMap.OnMyLocationClickListener { var1.onMyLocationClick(it) } })
-    }
 
-    override fun setOnMapLoadedCallback(var1: OnMapLoadedCallback?) {
+    override fun setOnMapLoadedCallback(var1: OnMapLoadedCallback?) =
         map.setOnMapLoadedCallback(var1?.let { GoogleMap.OnMapLoadedCallback { var1.onMapLoaded() } })
-    }
 
-    override fun setOnCircleClickListener(var1: OnCircleClickListener?) {
+    override fun setOnCircleClickListener(var1: OnCircleClickListener?) =
         map.setOnCircleClickListener(var1?.let { GoogleMap.OnCircleClickListener { var1.onCircleClick(GoogleCircle(it)) } })
-    }
 
-    override fun setOnPolygonClickListener(var1: OnPolygonClickListener?) {
+    override fun setOnPolygonClickListener(var1: OnPolygonClickListener?) =
         map.setOnPolygonClickListener(var1?.let { GoogleMap.OnPolygonClickListener { var1.onPolygonClick(GooglePolygon(it)) } })
-    }
 
-    override fun setOnPolylineClickListener(var1: OnPolylineClickListener?) {
+    override fun setOnPolylineClickListener(var1: OnPolylineClickListener?) =
         map.setOnPolylineClickListener(var1?.let { GoogleMap.OnPolylineClickListener { var1.onPolylineClick(GooglePolyline(it)) } })
-    }
 
-    override fun snapshot(var1: SnapshotReadyCallback) {
+    override fun snapshot(var1: SnapshotReadyCallback) =
         map.snapshot { var1.onSnapshotReady(it) }
-    }
 
-    override fun snapshot(var1: SnapshotReadyCallback, var2: Bitmap?) {
+    override fun snapshot(var1: SnapshotReadyCallback, var2: Bitmap?) =
         map.snapshot({ var1.onSnapshotReady(it) }, var2)
-    }
 
-    override fun setPadding(var1: Int, var2: Int, var3: Int, var4: Int) {
+    override fun setPadding(var1: Int, var2: Int, var3: Int, var4: Int) =
         map.setPadding(var1, var2, var3, var4)
-    }
 
-    override fun setContentDescription(var1: String?) {
+    override fun setContentDescription(var1: String?) =
         map.setContentDescription(var1)
-    }
 
-    override fun setOnPoiClickListener(var1: OnPoiClickListener?) {
+    override fun setOnPoiClickListener(var1: OnPoiClickListener?) =
         map.setOnPoiClickListener(var1?.let { GoogleMap.OnPoiClickListener { var1.onPoiClick(GooglePointOfInterest(it)) } })
-    }
 
-    override fun setMapStyle(var1: it.localhostsoftware.maps.model.MapStyleOptions<*>?): Boolean {
-        return map.setMapStyle(var1?.let { var1.mso as MapStyleOptions })
-    }
+    override fun setMapStyle(var1: it.localhostsoftware.maps.model.MapStyleOptions<*>?) =
+        map.setMapStyle(var1?.let { var1.mso as MapStyleOptions })
 
-    override fun setMinZoomPreference(var1: Float) {
+    override fun setMinZoomPreference(var1: Float) =
         map.setMinZoomPreference(var1)
-    }
 
-    override fun setMaxZoomPreference(var1: Float) {
+    override fun setMaxZoomPreference(var1: Float) =
         map.setMaxZoomPreference(var1)
-    }
 
-    override fun resetMinMaxZoomPreference() {
+    override fun resetMinMaxZoomPreference() =
         map.resetMinMaxZoomPreference()
-    }
 
-    override fun setLatLngBoundsForCameraTarget(var1: it.localhostsoftware.maps.model.LatLngBounds<*>?) {
+    override fun setLatLngBoundsForCameraTarget(var1: it.localhostsoftware.maps.model.LatLngBounds<*>?) =
         map.setLatLngBoundsForCameraTarget(var1?.let { var1.lb as LatLngBounds })
-    }
 }
--- maps/src/main/java/it/localhostsoftware/maps/huawei/HuaweiMap.kt
+++ maps/src/main/java/it/localhostsoftware/maps/huawei/HuaweiMap.kt
@@ -11,9 +11,8 @@ import it.localhostsoftware.maps.Projection
 import it.localhostsoftware.maps.UiSettings
 import it.localhostsoftware.maps.huawei.model.*
 import it.localhostsoftware.maps.model.MarkerOptions
-import it.localhostsoftware.maps.model.PolylineOptions
 
-class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, HuaweiCameraPosition>(huaweiMap) {
+class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, HuaweiCameraPosition, HuaweiPolylineOptions>(huaweiMap) {
     override val cameraPosition: HuaweiCameraPosition
         get() = HuaweiCameraPosition(map.cameraPosition)
     override val maxZoomLevel: Float
@@ -21,67 +20,56 @@ class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, Hu
     override val minZoomLevel: Float
         get() = map.minZoomLevel
 
-    override fun moveCamera(var1: HuaweiCameraUpdate) {
+    override fun moveCamera(var1: HuaweiCameraUpdate) =
         map.moveCamera(var1.cu)
-    }
 
-    override fun animateCamera(var1: HuaweiCameraUpdate) {
+    override fun animateCamera(var1: HuaweiCameraUpdate) =
         map.animateCamera(var1.cu)
-    }
 
-    override fun animateCamera(var1: HuaweiCameraUpdate, var2: CancelableCallback?) {
+    override fun animateCamera(var1: HuaweiCameraUpdate, var2: CancelableCallback?) =
         map.animateCamera(var1.cu, var2?.let {
             object : HuaweiMap.CancelableCallback {
                 override fun onFinish() = it.onFinish()
                 override fun onCancel() = it.onCancel()
             }
         })
-    }
 
-    override fun animateCamera(var1: HuaweiCameraUpdate, var2: Int, var3: CancelableCallback?) {
+    override fun animateCamera(var1: HuaweiCameraUpdate, var2: Int, var3: CancelableCallback?) =
         map.animateCamera(var1.cu, var2, var3?.let {
             object : HuaweiMap.CancelableCallback {
                 override fun onFinish() = it.onFinish()
                 override fun onCancel() = it.onCancel()
             }
         })
-    }
 
-    override fun stopAnimation() {
+    override fun stopAnimation() =
         map.stopAnimation()
-    }
 
-    override fun addPolyline(var1: PolylineOptions<*>): it.localhostsoftware.maps.model.Polyline<*> {
-        return HuaweiPolyline(map.addPolyline(var1.po as com.huawei.hms.maps.model.PolylineOptions))
-    }
+    override fun addPolyline(var1: HuaweiPolylineOptions) =
+        HuaweiPolyline(map.addPolyline(var1.po))
 
-    override fun addPolygon(var1: it.localhostsoftware.maps.model.PolygonOptions<*>): it.localhostsoftware.maps.model.Polygon<*> {
-        return HuaweiPolygon(map.addPolygon(var1.po as PolygonOptions))
-    }
+    override fun addPolygon(var1: it.localhostsoftware.maps.model.PolygonOptions<*>) =
+        HuaweiPolygon(map.addPolygon(var1.po as PolygonOptions))
 
-    override fun addCircle(var1: it.localhostsoftware.maps.model.CircleOptions<*>): it.localhostsoftware.maps.model.Circle<*> {
-        return HuaweiCircle(map.addCircle(var1.co as CircleOptions))
-    }
+    override fun addCircle(var1: it.localhostsoftware.maps.model.CircleOptions<*>) =
+        HuaweiCircle(map.addCircle(var1.co as CircleOptions))
 
-    override fun addMarker(var1: MarkerOptions<*>): it.localhostsoftware.maps.model.Marker<*> {
-        return HuaweiMarker(map.addMarker((var1 as HuaweiMarkerOptions).mo))
-    }
+    override fun addMarker(var1: MarkerOptions<*>) =
+        HuaweiMarker(map.addMarker((var1 as HuaweiMarkerOptions).mo))
 
-    override fun clear() {
+    override fun clear() =
         map.clear()
-    }
 
     override val focusedBuilding: it.localhostsoftware.maps.model.IndoorBuilding<*>
         get() = HuaweiIndoorBuilding(map.focusedBuilding)
 
-    override fun setOnIndoorStateChangeListener(var1: OnIndoorStateChangeListener?) {
+    override fun setOnIndoorStateChangeListener(var1: OnIndoorStateChangeListener?) =
         map.setOnIndoorStateChangeListener(var1?.let {
             object : HuaweiMap.OnIndoorStateChangeListener {
                 override fun onIndoorBuildingFocused() = var1.onIndoorBuildingFocused()
                 override fun onIndoorLevelActivated(indoorBuilding: IndoorBuilding) = var1.onIndoorLevelActivated(HuaweiIndoorBuilding(indoorBuilding))
             }
         })
-    }
 
     override var mapType: Int
         get() = map.mapType
@@ -96,9 +84,8 @@ class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, Hu
     override val isIndoorEnabled: Boolean
         get() = map.isIndoorEnabled
 
-    override fun setIndoorEnabled(var1: Boolean): Boolean {
-        return map.setIndoorEnabled(var1)
-    }
+    override fun setIndoorEnabled(var1: Boolean) =
+        map.setIndoorEnabled(var1)
 
     override var isBuildingsEnabled: Boolean
         get() = map.isBuildingsEnabled
@@ -113,7 +100,7 @@ class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, Hu
             map.isMyLocationEnabled = b
         }
 
-    override fun setLocationSource(var1: it.localhostsoftware.maps.LocationSource?) {
+    override fun setLocationSource(var1: it.localhostsoftware.maps.LocationSource?) =
         map.setLocationSource(var1?.let { ls ->
             object : LocationSource {
                 override fun deactivate() = ls.deactivate()
@@ -121,42 +108,34 @@ class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, Hu
                     var1.activate(it.localhostsoftware.maps.LocationSource.OnLocationChangedListener { onLocationChangedListener.onLocationChanged(it) })
             }
         })
-    }
 
     override val uiSettings: UiSettings<*>
         get() = HuaweiUiSettings(map.uiSettings)
     override val projection: Projection<*>
         get() = HuaweiProjection(map.projection)
 
-    override fun setOnCameraMoveStartedListener(var1: OnCameraMoveStartedListener?) {
+    override fun setOnCameraMoveStartedListener(var1: OnCameraMoveStartedListener?) =
         map.setOnCameraMoveStartedListener(var1?.let { HuaweiMap.OnCameraMoveStartedListener { var1.onCameraMoveStarted(it) } })
-    }
 
-    override fun setOnCameraMoveListener(var1: OnCameraMoveListener?) {
+    override fun setOnCameraMoveListener(var1: OnCameraMoveListener?) =
         map.setOnCameraMoveListener(var1?.let { HuaweiMap.OnCameraMoveListener { var1.onCameraMove() } })
-    }
 
-    override fun setOnCameraMoveCanceledListener(var1: OnCameraMoveCanceledListener?) {
+    override fun setOnCameraMoveCanceledListener(var1: OnCameraMoveCanceledListener?) =
         map.setOnCameraMoveCanceledListener(var1?.let { HuaweiMap.OnCameraMoveCanceledListener { var1.onCameraMoveCanceled() } })
-    }
 
-    override fun setOnCameraIdleListener(var1: OnCameraIdleListener?) {
+    override fun setOnCameraIdleListener(var1: OnCameraIdleListener?) =
         map.setOnCameraIdleListener(var1?.let { HuaweiMap.OnCameraIdleListener { var1.onCameraIdle() } })
-    }
 
-    override fun setOnMapClickListener(var1: OnMapClickListener?) {
+    override fun setOnMapClickListener(var1: OnMapClickListener?) =
         map.setOnMapClickListener(var1?.let { HuaweiMap.OnMapClickListener { var1.onMapClick(HuaweiLatLng(it)) } })
-    }
 
-    override fun setOnMapLongClickListener(var1: OnMapLongClickListener?) {
+    override fun setOnMapLongClickListener(var1: OnMapLongClickListener?) =
         map.setOnMapLongClickListener(var1?.let { HuaweiMap.OnMapLongClickListener { var1.onMapLongClick(HuaweiLatLng(it)) } })
-    }
 
-    override fun setOnMarkerClickListener(var1: OnMarkerClickListener?) {
+    override fun setOnMarkerClickListener(var1: OnMarkerClickListener?) =
         map.setOnMarkerClickListener(var1?.let { HuaweiMap.OnMarkerClickListener { var1.onMarkerClick(HuaweiMarker(it)) } })
-    }
 
-    override fun setOnMarkerDragListener(var1: OnMarkerDragListener?) {
+    override fun setOnMarkerDragListener(var1: OnMarkerDragListener?) =
         map.setOnMarkerDragListener(var1?.let {
             object : HuaweiMap.OnMarkerDragListener {
                 override fun onMarkerDragStart(marker: Marker) = var1.onMarkerDragStart(HuaweiMarker(marker))
@@ -164,91 +143,71 @@ class HuaweiMap(huaweiMap: HuaweiMap) : GeoMap<HuaweiMap, HuaweiCameraUpdate, Hu
                 override fun onMarkerDragEnd(marker: Marker) = var1.onMarkerDragEnd(HuaweiMarker(marker))
             }
         })
-    }
 
-    override fun setOnInfoWindowClickListener(var1: OnInfoWindowClickListener?) {
+    override fun setOnInfoWindowClickListener(var1: OnInfoWindowClickListener?) =
         map.setOnInfoWindowClickListener(var1?.let { HuaweiMap.OnInfoWindowClickListener { var1.onInfoWindowClick(HuaweiMarker(it)) } })
-    }
 
-    override fun setOnInfoWindowLongClickListener(var1: OnInfoWindowLongClickListener?) {
+    override fun setOnInfoWindowLongClickListener(var1: OnInfoWindowLongClickListener?) =
         map.setOnInfoWindowLongClickListener(var1?.let { HuaweiMap.OnInfoWindowLongClickListener { var1.onInfoWindowLongClick(HuaweiMarker(it)) } })
-    }
 
-    override fun setOnInfoWindowCloseListener(var1: OnInfoWindowCloseListener?) {
+    override fun setOnInfoWindowCloseListener(var1: OnInfoWindowCloseListener?) =
         map.setOnInfoWindowCloseListener(var1?.let { HuaweiMap.OnInfoWindowCloseListener { var1.onInfoWindowClose(HuaweiMarker(it)) } })
-    }
 
-    override fun setInfoWindowAdapter(var1: InfoWindowAdapter?) {
+    override fun setInfoWindowAdapter(var1: InfoWindowAdapter?) =
         map.setInfoWindowAdapter(var1?.let {
             object : HuaweiMap.InfoWindowAdapter {
                 override fun getInfoWindow(marker: Marker): View = it.getInfoWindow(HuaweiMarker(marker))
                 override fun getInfoContents(marker: Marker): View = it.getInfoContents(HuaweiMarker(marker))
             }
         })
-    }
 
-    override fun setOnMyLocationButtonClickListener(var1: OnMyLocationButtonClickListener?) {
+    override fun setOnMyLocationButtonClickListener(var1: OnMyLocationButtonClickListener?) =
         map.setOnMyLocationButtonClickListener(var1?.let { HuaweiMap.OnMyLocationButtonClickListener { var1.onMyLocationButtonClick() } })
-    }
 
-    override fun setOnMyLocationClickListener(var1: OnMyLocationClickListener?) {
+    override fun setOnMyLocationClickListener(var1: OnMyLocationClickListener?) =
         map.setOnMyLocationClickListener(var1?.let { HuaweiMap.OnMyLocationClickListener { var1.onMyLocationClick(it) } })
-    }
 
-    override fun setOnMapLoadedCallback(var1: OnMapLoadedCallback?) {
+    override fun setOnMapLoadedCallback(var1: OnMapLoadedCallback?) =
         map.setOnMapLoadedCallback(var1?.let { HuaweiMap.OnMapLoadedCallback { var1.onMapLoaded() } })
-    }
 
-    override fun setOnCircleClickListener(var1: OnCircleClickListener?) {
+    override fun setOnCircleClickListener(var1: OnCircleClickListener?) =
         map.setOnCircleClickListener(var1?.let { HuaweiMap.OnCircleClickListener { var1.onCircleClick(HuaweiCircle(it)) } })
-    }
 
-    override fun setOnPolygonClickListener(var1: OnPolygonClickListener?) {
+    override fun setOnPolygonClickListener(var1: OnPolygonClickListener?) =
         map.setOnPolygonClickListener(var1?.let { HuaweiMap.OnPolygonClickListener { var1.onPolygonClick(HuaweiPolygon(it)) } })
-    }
 
-    override fun setOnPolylineClickListener(var1: OnPolylineClickListener?) {
+    override fun setOnPolylineClickListener(var1: OnPolylineClickListener?) =
         map.setOnPolylineClickListener(var1?.let { HuaweiMap.OnPolylineClickListener { var1.onPolylineClick(HuaweiPolyline(it)) } })
-    }
 
-    override fun snapshot(var1: SnapshotReadyCallback) {
+    override fun snapshot(var1: SnapshotReadyCallback) =
         map.snapshot { var1.onSnapshotReady(it) }
-    }
 
-    override fun snapshot(var1: SnapshotReadyCallback, var2: Bitmap?) {
+    override fun snapshot(var1: SnapshotReadyCallback, var2: Bitmap?) =
         map.snapshot({ var1.onSnapshotReady(it) }, var2)
-    }
 
     override fun setPadding(var1: Int, var2: Int, var3: Int, var4: Int) {
         map.setPadding(var1, var2, var3, var4)
         map.uiSettings.setLogoPadding(var1, var2, var3, var4)
     }
 
-    override fun setContentDescription(var1: String?) {
+    override fun setContentDescription(var1: String?) =
         map.setContentDescription(var1)
-    }
 
-    override fun setOnPoiClickListener(var1: OnPoiClickListener?) {
+    override fun setOnPoiClickListener(var1: OnPoiClickListener?) =
         map.setOnPoiClickListener(var1?.let { HuaweiMap.OnPoiClickListener { var1.onPoiClick(HuaweiPointOfInterest(it)) } })
-    }
 
-    override fun setMapStyle(var1: it.localhostsoftware.maps.model.MapStyleOptions<*>?): Boolean {
-        return map.setMapStyle(var1?.let { var1.mso as MapStyleOptions })
-    }
+    override fun setMapStyle(var1: it.localhostsoftware.maps.model.MapStyleOptions<*>?) =
+        map.setMapStyle(var1?.let { var1.mso as MapStyleOptions })
 
-    override fun setMinZoomPreference(var1: Float) {
+    override fun setMinZoomPreference(var1: Float) =
         map.setMinZoomPreference(var1)
-    }
 
-    override fun setMaxZoomPreference(var1: Float) {
+    override fun setMaxZoomPreference(var1: Float) =
         map.setMaxZoomPreference(var1)
-    }
 
-    override fun resetMinMaxZoomPreference() {
+    override fun resetMinMaxZoomPreference() =
         map.resetMinMaxZoomPreference()
-    }
 
-    override fun setLatLngBoundsForCameraTarget(var1: it.localhostsoftware.maps.model.LatLngBounds<*>?) {
+    override fun setLatLngBoundsForCameraTarget(var1: it.localhostsoftware.maps.model.LatLngBounds<*>?) =
         map.setLatLngBoundsForCameraTarget(var1?.let { var1.lb as LatLngBounds })
-    }
 }
