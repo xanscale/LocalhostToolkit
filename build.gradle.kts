27634c8a248c2a00964586fb71b7f27be97de921
Alessandro Scarozza <xan.scale@gmail.com>
2023-05-09 17:48:05 +0200

    bump libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("android") version "1.8.21" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.library") version "8.0.1" apply false
 }
 
 tasks.register<Delete>("clean") {
