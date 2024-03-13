981f5f37dd56ce400bfc5a6104dd8dc4c3d1866d
Alessandro Scarozza <xan.scale@gmail.com>
2024-03-13 12:44:56 +0100

    bump libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("android") version "1.9.22" apply false
-    id("com.android.library") version "8.2.1" apply false
+    kotlin("android") version "1.9.23" apply false
+    id("com.android.library") version "8.3.0" apply false
 }
