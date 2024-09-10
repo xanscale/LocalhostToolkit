1981d9bf5c82847eaf7309285598f0de0ba8dd59
Alessandro Scarozza <xan.scale@gmail.com>
2024-09-10 17:26:52 +0200

    CircularProgressIndicator

--- core/src/main/java/it/localhostsoftware/core/app/fragment/ProgressDialogFragment.kt
+++ core/src/main/java/it/localhostsoftware/core/app/fragment/ProgressDialogFragment.kt
@@ -2,10 +2,10 @@ package it.localhostsoftware.core.app.fragment
 
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import it.localhostsoftware.core.R
 
 class ProgressDialogFragment : DialogFragment() {
@@ -23,7 +23,7 @@ class ProgressDialogFragment : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?) =
-        AlertDialog.Builder(requireContext(), R.style.ThemeOverlay_MaterialComponents_Dialog_Alert_App).setView(ProgressBar(requireContext())).create()
+        AlertDialog.Builder(requireContext(), R.style.ThemeOverlay_MaterialComponents_Dialog_Alert_App).setView(CircularProgressIndicator(requireContext()).apply { isIndeterminate = true }).create()
 
     override fun onDestroy() {
         requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
